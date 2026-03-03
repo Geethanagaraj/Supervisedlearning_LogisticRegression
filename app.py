e0b84fc4d1d494327f394cd70affbbe4d5e921a4
Geethanagaraj <geethanagaraj2006@gmail.com>
2026-03-03 18:35:20 +0530

    Step-4 : Train the model

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ y = data.targer
 #step 3
 x_train, x_test, y_train, y_test = train_test_split(test_size = 0.2, random_state = 42)
 
+#step 4
+model = LogisticRegression(max_iter = 5000)
+model.fit(x_train, y_train)
+
