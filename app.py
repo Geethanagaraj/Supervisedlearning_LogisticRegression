789728a1655bacaee7a74eb229f8a1342547b7c1
Geethanagaraj <geethanagaraj2006@gmail.com>
2026-03-03 18:31:25 +0530

    Step-1: Imported required libraries

---
+++ app.py
@@ -0,0 +1,5 @@
+#step 1
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.Linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
