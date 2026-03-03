2fef9bbad66e708965c06a24bf055c9da879df18
Geethanagaraj <geethanagaraj2006@gmail.com>
2026-03-03 18:32:58 +0530

    Step-2: Loaded the dataset

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.Linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
+
+#step 2
+data = load_breast_cancer()
+x = data.data
+y = data.targer
+
